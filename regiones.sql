387ddada1850f02e34a494737b1c610b4f6b3907
Bryan Peralta <bryperoliva@hotmail.com>
2015-07-25 19:50:37 -0300

    by Anahuel
    Regiones de Chile (15) por Id, nombre

---
+++ regiones.sql
@@ -0,0 +1,18 @@
+--<By Anahuel.Bryan Peralta> --
+INSERT INTO REGION (cod_region,nombre_region)
+VALUES
+(1,'Arica y Parinacota'),
+(2,'Tarapacá'),
+(3,'Antofagasta'),
+(4,'Atacama'),
+(5,'Coquimbo'),
+(6,'Valparaiso'),
+(7,'Metropolitana de Santiago'),
+(8,'OHiggins'),
+(9,'Maule'),
+(10,'Biobío'),
+(11,'La Araucanía'),
+(12,'Los Ríos'),
+(13,'Los Lagos'),
+(14,'Aisén del General Carlos Ibáñez del Campo'),
+(15,'Magallanes y de la Antártica Chilena');
